c04d6abef9f0ff7051ca01ec0f04b25c3049df4d
Anton <anton.gusevv.01@gmail.com>
2021-05-07 14:39:12 +0300

    добавлена автоматизация работы машины

--- TuringMachine/TuringMachine/MainForm.Designer.cs
+++ TuringMachine/TuringMachine/MainForm.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
             this.ButtonFront = new System.Windows.Forms.Button();
             this.ButtonBack = new System.Windows.Forms.Button();
             this.textBoxLine = new System.Windows.Forms.TextBox();
@@ -37,12 +38,14 @@
             this.label2 = new System.Windows.Forms.Label();
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.файлToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.ResetToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.EnterEmptyAgainToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
             this.SaveLineToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.SaveTableToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
             this.ShowInfoOnStartToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator3 = new System.Windows.Forms.ToolStripSeparator();
+            this.ResetToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.InfoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.ButtonAddColumns = new System.Windows.Forms.Button();
             this.ButtonDeleteColumns = new System.Windows.Forms.Button();
@@ -50,19 +53,26 @@
             this.buttonEraseLine = new System.Windows.Forms.Button();
             this.ButtonStep = new System.Windows.Forms.Button();
             this.saveFileDialog = new System.Windows.Forms.SaveFileDialog();
+            this.ButtonStart = new System.Windows.Forms.Button();
+            this.ButtonPause = new System.Windows.Forms.Button();
+            this.ButtonStop = new System.Windows.Forms.Button();
+            this.DelayNumericUpDown = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.DelayNumericUpDown)).BeginInit();
             this.SuspendLayout();
             // 
             // ButtonFront
             // 
             this.ButtonFront.Enabled = false;
             this.ButtonFront.Font = new System.Drawing.Font("Courier New", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.ButtonFront.Location = new System.Drawing.Point(710, 29);
+            this.ButtonFront.Image = ((System.Drawing.Image)(resources.GetObject("ButtonFront.Image")));
+            this.ButtonFront.Location = new System.Drawing.Point(724, 29);
             this.ButtonFront.Name = "ButtonFront";
-            this.ButtonFront.Size = new System.Drawing.Size(78, 46);
+            this.ButtonFront.Size = new System.Drawing.Size(64, 46);
             this.ButtonFront.TabIndex = 1;
-            this.ButtonFront.Text = "Вперед";
             this.ButtonFront.UseVisualStyleBackColor = true;
             this.ButtonFront.Click += new System.EventHandler(this.ButtonFront_Click);
             // 
@@ -70,11 +80,11 @@
             // 
             this.ButtonBack.Enabled = false;
             this.ButtonBack.Font = new System.Drawing.Font("Courier New", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.ButtonBack.Image = ((System.Drawing.Image)(resources.GetObject("ButtonBack.Image")));
             this.ButtonBack.Location = new System.Drawing.Point(12, 29);
             this.ButtonBack.Name = "ButtonBack";
-            this.ButtonBack.Size = new System.Drawing.Size(78, 46);
+            this.ButtonBack.Size = new System.Drawing.Size(64, 46);
             this.ButtonBack.TabIndex = 2;
-            this.ButtonBack.Text = "Назад";
             this.ButtonBack.UseVisualStyleBackColor = true;
             this.ButtonBack.Click += new System.EventHandler(this.ButtonBack_Click);
             // 
@@ -90,7 +100,7 @@
             // ButtonEnterLine
             // 
             this.ButtonEnterLine.Font = new System.Drawing.Font("Courier New", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.ButtonEnterLine.Location = new System.Drawing.Point(218, 30);
+            this.ButtonEnterLine.Location = new System.Drawing.Point(201, 29);
             this.ButtonEnterLine.Name = "ButtonEnterLine";
             this.ButtonEnterLine.Size = new System.Drawing.Size(78, 46);
             this.ButtonEnterLine.TabIndex = 5;
@@ -110,17 +120,16 @@
             // textBoxAlph
             // 
             this.textBoxAlph.Font = new System.Drawing.Font("Courier New", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.textBoxAlph.Location = new System.Drawing.Point(99, 50);
+            this.textBoxAlph.Location = new System.Drawing.Point(82, 49);
             this.textBoxAlph.Name = "textBoxAlph";
             this.textBoxAlph.Size = new System.Drawing.Size(113, 26);
             this.textBoxAlph.TabIndex = 8;
-            this.textBoxAlph.Text = "*";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Courier New", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label2.Location = new System.Drawing.Point(96, 29);
+            this.label2.Location = new System.Drawing.Point(82, 29);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(88, 18);
             this.label2.TabIndex = 9;
@@ -140,34 +149,36 @@
             // файлToolStripMenuItem
             // 
             this.файлToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.ResetToolStripMenuItem,
+            this.EnterEmptyAgainToolStripMenuItem,
             this.toolStripSeparator1,
             this.SaveLineToolStripMenuItem,
             this.SaveTableToolStripMenuItem,
             this.toolStripSeparator2,
-            this.ShowInfoOnStartToolStripMenuItem});
+            this.ShowInfoOnStartToolStripMenuItem,
+            this.toolStripSeparator3,
+            this.ResetToolStripMenuItem});
             this.файлToolStripMenuItem.Font = new System.Drawing.Font("Courier New", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.файлToolStripMenuItem.Name = "файлToolStripMenuItem";
             this.файлToolStripMenuItem.Size = new System.Drawing.Size(60, 22);
             this.файлToolStripMenuItem.Text = "Файл";
             // 
-            // ResetToolStripMenuItem
+            // EnterEmptyAgainToolStripMenuItem
             // 
-            this.ResetToolStripMenuItem.Name = "ResetToolStripMenuItem";
-            this.ResetToolStripMenuItem.Size = new System.Drawing.Size(376, 22);
-            this.ResetToolStripMenuItem.Text = "Перезапуск";
-            this.ResetToolStripMenuItem.Click += new System.EventHandler(this.ResetToolStripMenuItem_Click);
+            this.EnterEmptyAgainToolStripMenuItem.Name = "EnterEmptyAgainToolStripMenuItem";
+            this.EnterEmptyAgainToolStripMenuItem.Size = new System.Drawing.Size(456, 22);
+            this.EnterEmptyAgainToolStripMenuItem.Text = "Указать количество пустых ячеек заново";
+            this.EnterEmptyAgainToolStripMenuItem.Click += new System.EventHandler(this.EnterEmptyAgainToolStripMenuItem_Click);
             // 
             // toolStripSeparator1
             // 
             this.toolStripSeparator1.Name = "toolStripSeparator1";
-            this.toolStripSeparator1.Size = new System.Drawing.Size(373, 6);
+            this.toolStripSeparator1.Size = new System.Drawing.Size(453, 6);
             // 
             // SaveLineToolStripMenuItem
             // 
             this.SaveLineToolStripMenuItem.Enabled = false;
             this.SaveLineToolStripMenuItem.Name = "SaveLineToolStripMenuItem";
-            this.SaveLineToolStripMenuItem.Size = new System.Drawing.Size(376, 22);
+            this.SaveLineToolStripMenuItem.Size = new System.Drawing.Size(456, 22);
             this.SaveLineToolStripMenuItem.Text = "Сохранить ленту";
             this.SaveLineToolStripMenuItem.Click += new System.EventHandler(this.SaveLineToolStripMenuItem_Click);
             // 
@@ -175,23 +186,35 @@
             // 
             this.SaveTableToolStripMenuItem.Enabled = false;
             this.SaveTableToolStripMenuItem.Name = "SaveTableToolStripMenuItem";
-            this.SaveTableToolStripMenuItem.Size = new System.Drawing.Size(376, 22);
+            this.SaveTableToolStripMenuItem.Size = new System.Drawing.Size(456, 22);
             this.SaveTableToolStripMenuItem.Text = "Сохранить таблицу состояний";
             this.SaveTableToolStripMenuItem.Click += new System.EventHandler(this.SaveTableToolStripMenuItem_Click);
             // 
             // toolStripSeparator2
             // 
             this.toolStripSeparator2.Name = "toolStripSeparator2";
-            this.toolStripSeparator2.Size = new System.Drawing.Size(373, 6);
+            this.toolStripSeparator2.Size = new System.Drawing.Size(453, 6);
             // 
             // ShowInfoOnStartToolStripMenuItem
             // 
             this.ShowInfoOnStartToolStripMenuItem.Checked = true;
             this.ShowInfoOnStartToolStripMenuItem.CheckState = System.Windows.Forms.CheckState.Checked;
             this.ShowInfoOnStartToolStripMenuItem.Name = "ShowInfoOnStartToolStripMenuItem";
-            this.ShowInfoOnStartToolStripMenuItem.Size = new System.Drawing.Size(376, 22);
+            this.ShowInfoOnStartToolStripMenuItem.Size = new System.Drawing.Size(456, 22);
             this.ShowInfoOnStartToolStripMenuItem.Text = "Показывать справку при запуске";
             this.ShowInfoOnStartToolStripMenuItem.Click += new System.EventHandler(this.ShowInfoOnStartToolStripMenuItem_Click);
+            // 
+            // toolStripSeparator3
+            // 
+            this.toolStripSeparator3.Name = "toolStripSeparator3";
+            this.toolStripSeparator3.Size = new System.Drawing.Size(453, 6);
+            // 
+            // ResetToolStripMenuItem
+            // 
+            this.ResetToolStripMenuItem.Name = "ResetToolStripMenuItem";
+            this.ResetToolStripMenuItem.Size = new System.Drawing.Size(456, 22);
+            this.ResetToolStripMenuItem.Text = "Перезапуск";
+            this.ResetToolStripMenuItem.Click += new System.EventHandler(this.ResetToolStripMenuItem_Click);
             // 
             // InfoToolStripMenuItem
             // 
@@ -237,7 +260,7 @@
             // buttonEraseLine
             // 
             this.buttonEraseLine.Font = new System.Drawing.Font("Courier New", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.buttonEraseLine.Location = new System.Drawing.Point(302, 30);
+            this.buttonEraseLine.Location = new System.Drawing.Point(285, 29);
             this.buttonEraseLine.Name = "buttonEraseLine";
             this.buttonEraseLine.Size = new System.Drawing.Size(92, 46);
             this.buttonEraseLine.TabIndex = 14;
@@ -249,19 +272,98 @@
             // 
             this.ButtonStep.Enabled = false;
             this.ButtonStep.Font = new System.Drawing.Font("Courier New", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.ButtonStep.Location = new System.Drawing.Point(512, 30);
+            this.ButtonStep.Image = ((System.Drawing.Image)(resources.GetObject("ButtonStep.Image")));
+            this.ButtonStep.Location = new System.Drawing.Point(566, 29);
             this.ButtonStep.Name = "ButtonStep";
-            this.ButtonStep.Size = new System.Drawing.Size(92, 46);
+            this.ButtonStep.Size = new System.Drawing.Size(55, 46);
             this.ButtonStep.TabIndex = 15;
-            this.ButtonStep.Text = "Шаг";
             this.ButtonStep.UseVisualStyleBackColor = true;
             this.ButtonStep.Click += new System.EventHandler(this.ButtonStep_Click);
+            // 
+            // ButtonStart
+            // 
+            this.ButtonStart.Enabled = false;
+            this.ButtonStart.Font = new System.Drawing.Font("Courier New", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.ButtonStart.Image = ((System.Drawing.Image)(resources.GetObject("ButtonStart.Image")));
+            this.ButtonStart.Location = new System.Drawing.Point(383, 29);
+            this.ButtonStart.Name = "ButtonStart";
+            this.ButtonStart.Size = new System.Drawing.Size(55, 46);
+            this.ButtonStart.TabIndex = 16;
+            this.ButtonStart.UseVisualStyleBackColor = true;
+            this.ButtonStart.Click += new System.EventHandler(this.ButtonStart_Click);
+            // 
+            // ButtonPause
+            // 
+            this.ButtonPause.Enabled = false;
+            this.ButtonPause.Font = new System.Drawing.Font("Courier New", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.ButtonPause.Image = ((System.Drawing.Image)(resources.GetObject("ButtonPause.Image")));
+            this.ButtonPause.Location = new System.Drawing.Point(444, 29);
+            this.ButtonPause.Name = "ButtonPause";
+            this.ButtonPause.Size = new System.Drawing.Size(55, 46);
+            this.ButtonPause.TabIndex = 17;
+            this.ButtonPause.UseVisualStyleBackColor = true;
+            this.ButtonPause.Click += new System.EventHandler(this.ButtonPause_Click);
+            // 
+            // ButtonStop
+            // 
+            this.ButtonStop.Enabled = false;
+            this.ButtonStop.Font = new System.Drawing.Font("Courier New", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.ButtonStop.Image = ((System.Drawing.Image)(resources.GetObject("ButtonStop.Image")));
+            this.ButtonStop.Location = new System.Drawing.Point(505, 29);
+            this.ButtonStop.Name = "ButtonStop";
+            this.ButtonStop.Size = new System.Drawing.Size(55, 46);
+            this.ButtonStop.TabIndex = 18;
+            this.ButtonStop.UseVisualStyleBackColor = true;
+            this.ButtonStop.Click += new System.EventHandler(this.ButtonStop_Click);
+            // 
+            // DelayNumericUpDown
+            // 
+            this.DelayNumericUpDown.Font = new System.Drawing.Font("Courier New", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.DelayNumericUpDown.Increment = new decimal(new int[] {
+            50,
+            0,
+            0,
+            0});
+            this.DelayNumericUpDown.Location = new System.Drawing.Point(628, 49);
+            this.DelayNumericUpDown.Maximum = new decimal(new int[] {
+            5000,
+            0,
+            0,
+            0});
+            this.DelayNumericUpDown.Minimum = new decimal(new int[] {
+            100,
+            0,
+            0,
+            0});
+            this.DelayNumericUpDown.Name = "DelayNumericUpDown";
+            this.DelayNumericUpDown.Size = new System.Drawing.Size(90, 26);
+            this.DelayNumericUpDown.TabIndex = 19;
+            this.DelayNumericUpDown.Value = new decimal(new int[] {
+            100,
+            0,
+            0,
+            0});
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Courier New", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(625, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(98, 18);
+            this.label1.TabIndex = 20;
+            this.label1.Text = "Задержка:";
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 371);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.DelayNumericUpDown);
+            this.Controls.Add(this.ButtonStop);
+            this.Controls.Add(this.ButtonPause);
+            this.Controls.Add(this.ButtonStart);
             this.Controls.Add(this.ButtonStep);
             this.Controls.Add(this.buttonEraseLine);
             this.Controls.Add(this.textBoxPointer);
@@ -282,6 +384,7 @@
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.DelayNumericUpDown)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -310,6 +413,14 @@
         private System.Windows.Forms.ToolStripSeparator toolStripSeparator2;
         private System.Windows.Forms.ToolStripMenuItem ShowInfoOnStartToolStripMenuItem;
         private System.Windows.Forms.SaveFileDialog saveFileDialog;
+        private System.Windows.Forms.ToolStripMenuItem EnterEmptyAgainToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator3;
+        private System.Windows.Forms.Button ButtonStart;
+        private System.Windows.Forms.Button ButtonPause;
+        private System.Windows.Forms.Button ButtonStop;
+        private System.Windows.Forms.NumericUpDown DelayNumericUpDown;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
     }
 }
 
--- TuringMachine/TuringMachine/MainForm.cs
+++ TuringMachine/TuringMachine/MainForm.cs
@@ -1,12 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.OleDb;
+using System.Reflection;
 
 namespace TuringMachine
 {
-       
+
     public partial class MainForm : Form
     {
         public static ProcessWorkingMachine work = new ProcessWorkingMachine();
@@ -19,6 +23,8 @@ namespace TuringMachine
         {
             InitializeComponent();
             MaximizeBox = false;
+            saveFileDialog.Filter = "Text (*.txt)|*.txt";
+            openFileDialog.Filter = "Excel (*.XLS)|*.XLS";
 
             if (Settings.Default.Show == true)
             {
@@ -110,6 +116,7 @@ namespace TuringMachine
                 LineList.Add(textBoxLine.Text[i].ToString());
             }
 
+            Alph.Add("*");
             for (int i = 0; i < textBoxAlph.TextLength; i++) // алфавит лист
             {
                 Alph.Add(textBoxAlph.Text[i].ToString());
@@ -146,6 +153,7 @@ namespace TuringMachine
                 SaveTableToolStripMenuItem.Enabled = true;
 
                 ButtonStep.Enabled = true;
+                ButtonStart.Enabled = true;
 
             }
             else if (counter != 0)
@@ -237,7 +245,9 @@ namespace TuringMachine
         private void buttonEraseLine_Click(object sender, EventArgs e) // стереть ленту
         {
             ButtonEnterLine.Enabled = true; // разблок кнопки ввести ленту
-            ButtonStep.Enabled = false;
+            ButtonStep.Enabled = false; // блок управляющих кнопок
+            ButtonStart.Enabled = false;
+
             textBoxLine.Text = ""; // очиска ленты
             for (int i = 0; i < work.CountEmpty; i++)
             {
@@ -290,7 +300,6 @@ namespace TuringMachine
 
             try
             {
-
                 work.CurrentContentCell = LineList[pointerPosition]; //текущая клетка - значение ленты где стоит указатель снизу
                 work.Command = (string)dataGridView1[work.NextColumn, Alph.IndexOf(work.CurrentContentCell)].Value; // берем команду из ячейки таблицы
                 work.SplittedCommand = work.Command.Split(' ').ToList(); // сплитим ее по пробелам
@@ -322,6 +331,8 @@ namespace TuringMachine
                 else if (work.Direction == "H")// стоп машина
                 {
                     MessageBox.Show("Выполнение программы завершено");
+                    ButtonStart.Enabled = true;
+                    ButtonStop_Click(null, null);
                 }
                 else
                 {
@@ -333,24 +344,28 @@ namespace TuringMachine
                 string textError = "Ячейка (q" + work.NextColumn + ";" + LineList[pointerPosition] + ") не содержит команды";
                 dataGridView1.Rows[Alph.IndexOf(work.CurrentContentCell)].Cells[int.Parse(work.NextColumn)].Style.BackColor = Color.Red;
                 MessageBox.Show(textError, "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                ButtonStart.Enabled = true;
             }
             catch (AlphabetException)
             {
                 string textError = "Ячейка (q" + work.NextColumn + ";" + LineList[pointerPosition] + ") содержит необъявленный в алфавите символ. Замена на ленте невозможна.";
                 dataGridView1.Rows[Alph.IndexOf(work.CurrentContentCell)].Cells[int.Parse(work.NextColumn)].Style.BackColor = Color.Red;
                 MessageBox.Show(textError, "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                ButtonStart.Enabled = true;
             }
             catch (WrongDirectionException)
             {
                 string textError = "Ячейка (q" + work.NextColumn + ";" + LineList[pointerPosition] + ") содержит неверное значение направления сдвига по ленте. Сдвиг по ленте невозможен.";
                 dataGridView1.Rows[Alph.IndexOf(work.CurrentContentCell)].Cells[int.Parse(work.NextColumn)].Style.BackColor = Color.Red;
                 MessageBox.Show(textError, "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                ButtonStart.Enabled = true;
             }
             catch (NextColumnException)
             {
                 string textError = "Ячейка (q" + work.NextColumn + ";" + LineList[pointerPosition] + ") содержит номер несуществующего состояния. Переход в указанное состояние невозможен";
                 dataGridView1.Rows[Alph.IndexOf(work.CurrentContentCell)].Cells[int.Parse(work.NextColumn)].Style.BackColor = Color.Red;
                 MessageBox.Show(textError, "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                ButtonStart.Enabled = true;
             }
 
         }
@@ -423,18 +438,76 @@ namespace TuringMachine
             ExcelApp.Visible = true;
             ExcelApp.UserControl = true;
         }
-    }
-    // область описания пользовательских исключений
-    public class AlphabetException : ApplicationException
-    {
-        public AlphabetException() : base() { }
-    }
-    public class WrongDirectionException : ApplicationException
-    {
-        public WrongDirectionException() : base() { }
-    }
-    public class NextColumnException : ApplicationException
-    {
-        public NextColumnException() : base() { }
+
+        private async void ButtonStart_Click(object sender, EventArgs e)
+        {
+            ButtonStart.Enabled = false;
+
+            ButtonPause.Enabled = true;
+            ButtonStop.Enabled = true;
+
+            int delay = int.Parse(DelayNumericUpDown.Text);
+            while (!ButtonStart.Enabled)
+            {
+                ButtonStep_Click(null, null);
+                await Task.Delay(delay);
+            }
+        }
+
+        private void ButtonPause_Click(object sender, EventArgs e)
+        {
+            ButtonStart.Enabled = true;
+            ButtonPause.Enabled = false;
+        }
+
+        private void ButtonStop_Click(object sender, EventArgs e)
+        {
+            foreach (DataGridViewRow row in dataGridView1.Rows)
+            {
+                foreach (DataGridViewCell cell in row.Cells)
+                {
+                    cell.Style.BackColor = Color.White; // красим в белый
+                }
+            }
+
+            pointerPosition = work.CountEmpty - 1;
+
+            work.Command = null; work.Direction = null;
+            work.NextColumn = null; work.ReplaceOnIt = null;
+
+            ButtonStart.Enabled = true;
+            ButtonPause.Enabled = false;
+            ButtonStop.Enabled = false;
+
+        }
+
+        private void EnterEmptyAgainToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            buttonEraseLine_Click(null, null);
+            work.CountEmpty = 0;
+            var f = new Empty(work);
+            f.ShowDialog();
+
+            if (work.CountEmpty == 0) work.CountEmpty = 1;
+            for (int i = 0; i < work.CountEmpty; i++)
+            {
+                textBoxLine.Text += "*";
+            }
+        }
+
+        // область описания пользовательских исключений
+        public class AlphabetException : ApplicationException
+        {
+            public AlphabetException() : base() { }
+        }
+        public class WrongDirectionException : ApplicationException
+        {
+            public WrongDirectionException() : base() { }
+        }
+        public class NextColumnException : ApplicationException
+        {
+            public NextColumnException() : base() { }
+        }
+
     }
 }
